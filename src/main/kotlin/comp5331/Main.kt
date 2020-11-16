89837b8ec7be07eeadb03a639d1054da4dbec347
David Mak <david.18.19.21@gmail.com>
2020-11-16 17:34:30 +0800

    Add subcommands to transform dataset to various formats

--- src/main/kotlin/comp5331/Main.kt
+++ src/main/kotlin/comp5331/Main.kt
@@ -1,15 +1,18 @@
 package comp5331
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.subcommands
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.int
 import com.github.ajalt.clikt.parameters.types.path
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 import model.RepositoryOutputFormat
+import java.io.BufferedWriter
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 import java.nio.file.Path
@@ -18,6 +21,11 @@ import java.nio.file.StandardOpenOption
 import java.util.concurrent.CompletableFuture
 
 class MainCmd : CliktCommand() {
+
+    override fun run() = Unit
+}
+
+class FetchCmd : CliktCommand() {
 
     private val token: String? by option(help = "Github Token")
     private val query: String? by option(help = "Github Query String Override")
@@ -62,11 +70,128 @@ class MainCmd : CliktCommand() {
                 } else it
             }
 
-        Files.newBufferedWriter(output, StandardCharsets.UTF_8, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)
+        Files.newBufferedWriter(
+            output,
+            StandardCharsets.UTF_8,
+            StandardOpenOption.CREATE,
+            StandardOpenOption.TRUNCATE_EXISTING
+        )
             .use {
                 it.write(jsonAdapter.toJson(outData))
             }
     }
 }
 
-fun main(args: Array<String>) = MainCmd().main(args)
+class TransformDocumentCmd :
+    CliktCommand(help = "Transforms a collected dataset to a JSON object per line.", name = "transform-doc") {
+
+    private val input: Path by option(help = "Path to input JSON")
+        .path(mustExist = true, canBeDir = false, mustBeReadable = true)
+        .required()
+    private val output: Path by option(help = "Path to output JSON")
+        .path()
+        .required()
+
+    override fun run() {
+        val moshi = Moshi.Builder().build()
+        val type = Types.newParameterizedType(List::class.java, RepositoryOutputFormat::class.java)
+        val oldJsonAdapter = moshi.adapter<List<RepositoryOutputFormat>>(type)
+
+        val hier = checkNotNull(oldJsonAdapter.fromJson(Files.readString(input)))
+//        val newHier = hier.map {
+//            it.copy(text = it.text)
+//        }
+
+        val newJsonAdapter = moshi.adapter(RepositoryOutputFormat::class.java)
+
+        Files.newBufferedWriter(
+            output,
+            StandardCharsets.UTF_8,
+            StandardOpenOption.CREATE,
+            StandardOpenOption.TRUNCATE_EXISTING
+        )
+            .use { writer ->
+                hier.forEach {
+//                newHier.forEach {
+                    writer.write(newJsonAdapter.toJson(it))
+                    writer.newLine()
+                }
+            }
+    }
+}
+
+class TransformDatasetCmd :
+    CliktCommand(help = "Emits necessary text files based on a collected JSON file.", name = "transform-dataset") {
+
+    private val input: Path by option(help = "Path to input JSON")
+        .path(mustExist = true, canBeDir = false, mustBeReadable = true)
+        .required()
+    private val outputDir: Path by option(help = "Directory to output files")
+        .path(mustExist = true, canBeFile = false, mustBeWritable = true)
+        .default(Paths.get(""))
+
+    override fun run() {
+        val moshi = Moshi.Builder().build()
+        val jsonAdapter = moshi.adapter(RepositoryOutputFormat::class.java)
+
+        val datasetPath = outputDir.resolve(DATASET_FILENAME)
+        val labelsPath = outputDir.resolve(LABELS_FILENAME)
+
+        Files.deleteIfExists(datasetPath)
+        Files.deleteIfExists(labelsPath)
+
+        var datasetWriter: BufferedWriter? = null
+        var labelsWriter: BufferedWriter? = null
+        try {
+            datasetWriter = Files.newBufferedWriter(datasetPath)
+            labelsWriter = Files.newBufferedWriter(labelsPath)
+
+            Files.lines(input)
+                .use { lines ->
+                    lines.forEach { line ->
+                        val inDataset = checkNotNull(jsonAdapter.fromJson(line))
+                        val document = inDataset.asDocument()
+
+                        checkNotNull(datasetWriter).let {
+                            it.write(document)
+                            it.newLine()
+                        }
+                        checkNotNull(labelsWriter).let {
+                            it.write(requireNotNull(inDataset.sub_label))
+                            it.newLine()
+                        }
+                    }
+                }
+        } finally {
+            datasetWriter?.close()
+            labelsWriter?.close()
+        }
+
+        Files.newBufferedWriter(
+            Paths.get("", "dataset.txt"),
+            StandardCharsets.UTF_8,
+            StandardOpenOption.CREATE,
+            StandardOpenOption.TRUNCATE_EXISTING
+        ).use { writer ->
+            Files.lines(input)
+                .use { lines ->
+                    lines.forEach {
+                        val inDataset = checkNotNull(jsonAdapter.fromJson(it))
+                        val line = inDataset.asDocument()
+
+                        writer.write(line)
+                        writer.newLine()
+                    }
+                }
+        }
+    }
+
+    companion object {
+        private const val DATASET_FILENAME = "dataset.txt"
+        private const val LABELS_FILENAME = "labels.txt"
+    }
+}
+
+fun main(args: Array<String>) = MainCmd()
+    .subcommands(FetchCmd(), TransformDatasetCmd(), TransformDocumentCmd())
+    .main(args)
--- src/main/kotlin/model/RepositoryOutputFormat.kt
+++ src/main/kotlin/model/RepositoryOutputFormat.kt
@@ -14,16 +14,33 @@ data class RepositoryOutputFormat(
     val repo_name: String
 ) {
 
+    fun asDocument(): String {
+        return arrayOf(
+            tags.joinToString(" "),
+            repo_name_seg,
+            user
+        ).joinToString(" ")
+    }
+
     companion object {
+
+//        fun transformText(input: String): String {
+//            return input
+//                .replace("\n", " ")
+//                .replace(Regex("([^a-zA-Z\\d\\s])([a-zA-Z\\d\\s])")) { "${it.groupValues[1]} ${it.groupValues[2]}" }
+//                .replace(Regex("([a-zA-Z\\d\\s])([^a-zA-Z\\d\\s])")) { "${it.groupValues[1]} ${it.groupValues[2]}" }
+//        }
+
         fun fromGithub(repository: Repository, topics: List<String>, readme: String?): RepositoryOutputFormat {
             return RepositoryOutputFormat(
-                    user = repository.owner.login,
-                    text = arrayOf(repository.description, readme).filterNotNull().joinToString(" "),
-                    tags = topics,
-                    super_label = null,
-                    sub_label = null,
-                    repo_name_seg = repository.name.replace(Regex("[^a-zA-Z\\d]"), " "),
-                    repo_name = repository.full_name
+                user = repository.owner.login,
+                text = arrayOf(repository.description, readme).filterNotNull().joinToString(" "),
+//                text = transformText(arrayOf(repository.description, readme).filterNotNull().joinToString(" ")),
+                tags = topics,
+                super_label = null,
+                sub_label = null,
+                repo_name_seg = repository.name.replace(Regex("[^a-zA-Z\\d]"), " "),
+                repo_name = repository.full_name
             )
         }
     }
